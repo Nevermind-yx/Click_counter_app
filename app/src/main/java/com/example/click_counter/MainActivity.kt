494e7f8fd755cc09d4593f8e4ace4612ccd124b5
Nevermind <lambojon.gabrieljohn@gmail.com>
2025-12-27 21:32:04 +0800

    Try

--- app/src/main/java/com/example/click_counter/MainActivity.kt
+++ app/src/main/java/com/example/click_counter/MainActivity.kt
@@ -1,20 +1,22 @@
-package com.example.click_counter
+package com.example.click_counter // MAKE SURE THIS MATCHES YOUR FILE
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvCount = findViewById<TextView>(R.id.tvCount)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        var count = 0
+
+        btnAdd.setOnClickListener {
+            count++
+            tvCount.text = count.toString()
         }
     }
 }
